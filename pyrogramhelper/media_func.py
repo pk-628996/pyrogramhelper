de0b037f9a440f6626f377a8b0ac4f5dd3c89d4d
naipk <105729735+naipk@users.noreply.github.com>
2022-10-15 12:32:30 +0530

    Update media_func.py

--- pyrogramhelper/media_func.py
+++ pyrogramhelper/media_func.py
@@ -1,1 +1,10 @@
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 
+def durationfunc(file):
+ metadata = extractMetadata(createParser(f"""{file}"""))
+ if metadata.has("duration"):
+   duration = metadata.get('duration').seconds
+ else:
+     duration = 10
+ return duration
